8c86215a2a1d0cadbc513347b128fcda3b859868
JosGielen <joseph.gielen1@telenet.be>
2023-05-25 21:09:33 +0200

    Uploading Files

---
+++ Cabana/Marching Squares/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Marching_Squares
+{
+
+    public partial class MainWindow : Window
+    {
+        private WriteableBitmap writeableBmp;
+        private readonly int rez = 2;
+        private int cols;
+        private int rows;
+        private double Xoff;
+        private double Yoff;
+        private double Zoff;
+        private double[,] field;
+        private int[,] intField;
+        private Vector[,] locations;
+        private bool filled = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            writeableBmp = BitmapFactory.New((int)UserArea.ActualWidth, (int)UserArea.ActualHeight);
+            Image1.Source = writeableBmp;
+            cols = writeableBmp.PixelWidth / rez + 1;
+            rows = writeableBmp.PixelWidth / rez + 1;
+            field = new double[cols, rows];
+            intField = new int[cols, rows];
+            locations = new Vector[cols, rows];
+            Xoff = 0;
+            Yoff = 0;
+            Zoff = 0;
+            for (int I = 0; I < cols; I++)
+            {
+                for (int J = 0; J < rows; J++)
+                {
+                    locations[I, J] = new Vector(I * rez, J * rez);
+                }
+            }
+            CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            using (writeableBmp.GetBitmapContext())
+            {
+                writeableBmp.Clear();
+                //Fill the field with OpenSimplexNoise
+                Xoff = 0;
+                for (int I = 0; I < cols; I++)
+                {
+                    Yoff = 0;
+                    for (int J = 0; J < rows; J++)
+                    {
+                        field[I, J] = FastSimplexNoise.Noise3D(Xoff, Yoff, Zoff);
+                        if (field[I, J] < 0.5)
+                        {
+                            intField[I, J] = 0;
+                        }
+                        else
+                        {
+                            intField[I, J] = 1;
+                        }
+                        if (filled) writeableBmp.FillRectangle((int)((I - 0.5) * rez), (int)((J - 0.5) * rez), (int)((I + 0.5) * rez), (int)((J + 0.5) * rez), Color.FromRgb((byte)(255 * field[I, J]), (byte)(255 * field[I, J]), (byte)(255 * field[I, J])));
+                        Yoff += 0.03;
+                    }
+                    Xoff += 0.03;
+                }
+                Zoff += 0.006;
+
+                //Marching Squares
+                Marching_Square MS;
+                Point topLeft;
+                Point bottomRight;
+                for (int I = 0; I < cols - 1; I++)
+                {
+                    for (int J = 0; J < rows - 1; J++)
+                    {
+                        topLeft = new Point((I - 0.5) * rez, (J - 0.5) * rez);
+                        bottomRight = new Point((I + 0.5) * rez, (J + 0.5) * rez);
+                        MS = new Marching_Square(topLeft, bottomRight);
+                        foreach (Line l in MS.GetLines(intField[I, J], intField[I + 1, J], intField[I + 1, J + 1], intField[I, J + 1]))
+                        {
+                            //Draw 2 lines to make a better visible linethickness 2
+                            writeableBmp.DrawLine((int)l.X1, (int)l.Y1, (int)l.X2, (int)l.Y2, Colors.Red);
+                            writeableBmp.DrawLine((int)l.X1, (int)l.Y1 + 1, (int)l.X2, (int)l.Y2 + 1, Colors.Red);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Cabana/Marching Squares/Marching Square.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Marching_Squares
+{
+    class Marching_Square
+    {
+        private Vector my_TopLeft;
+        private Vector my_TopRight;
+        private Vector my_BottomLeft;
+        private Vector my_BottomRight;
+
+        /// <summary>
+        /// Creates a Marching Square
+        /// </summary>
+        /// <param name="Location">A Rectangle specifying the Top, Left position and With, Height of the Marching Square.</param>
+        public Marching_Square(Rect location)
+        {
+            my_TopLeft = new Vector(location.Left, location.Top);
+            my_TopRight = new Vector(location.Right, location.Top);
+            my_BottomLeft = new Vector(location.Left, location.Bottom);
+            my_BottomRight = new Vector(location.Right, location.Bottom);
+        }
+
+        public Marching_Square(double left, double top, double width, double height)
+        {
+            my_TopLeft = new Vector(left, top);
+            my_TopRight = new Vector(left + width, top);
+            my_BottomLeft = new Vector(left, top + height);
+            my_BottomRight = new Vector(left + width, top + height);
+        }
+
+        public Marching_Square(Vector topLeft, Vector topRight, Vector bottomLeft, Vector bottomRight)
+        {
+            my_TopLeft = topLeft;
+            my_TopRight = topRight;
+            my_BottomLeft = bottomLeft;
+            my_BottomRight = bottomRight;
+        }
+
+        public Marching_Square(Vector topLeft, Vector bottomRight)
+        {
+            my_TopLeft = new Vector(topLeft.X, topLeft.Y);
+            my_TopRight = new Vector(bottomRight.X, topLeft.Y);
+            my_BottomLeft = new Vector(topLeft.X, bottomRight.Y);
+            my_BottomRight = new Vector(bottomRight.X, bottomRight.Y);
+        }
+
+        public Marching_Square(Point topLeft, Point topRight, Point bottomLeft, Point bottomRight)
+        {
+            my_TopLeft = new Vector(topLeft.X, topLeft.Y);
+            my_TopRight = new Vector(topRight.X, topRight.Y);
+            my_BottomLeft = new Vector(bottomLeft.X, bottomLeft.Y);
+            my_BottomRight = new Vector(bottomRight.X, bottomRight.Y);
+        }
+
+        public Marching_Square(Point topLeft, Point bottomRight)
+        {
+            my_TopLeft = new Vector(topLeft.X, topLeft.Y);
+            my_TopRight = new Vector(bottomRight.X, topLeft.Y);
+            my_BottomLeft = new Vector(topLeft.X, bottomRight.Y);
+            my_BottomRight = new Vector(bottomRight.X, bottomRight.Y);
+        }
+
+        /// <summary>
+        /// Returns 1 or 2 lines defined by the marching square with the 4 given corner values
+        /// </summary>
+        /// <param name="CornerValue1">TopLeft Corner Value must be 0 or 1</param>
+        /// <param name="cornerValue2">TopRight Corner Value must be 0 or 1</param>
+        /// <param name="cornerValue3">BottomLeft Corner Value must be 0 or 1</param>
+        /// <param name="cornerValue4">BottomRight Corner Value must be 0 or 1</param>
+        /// <returns></returns>
+        public List<Line> GetLines(int CornerValue1, int CornerValue2, int CornerValue3, int CornerValue4)
+        {
+            List<Line> result = new List<Line>();
+            Line l;
+            int squareType;
+            Vector A;
+            Vector B;
+            squareType = 8 * CornerValue1 + 4 * CornerValue2 + 2 * CornerValue3 + CornerValue4;
+            switch (squareType)
+            {
+                case 0:
+                    //Do nothing
+                    break;
+                case 1:
+                    A = 0.5 * (my_TopLeft + my_BottomLeft);
+                    B = 0.5 * (my_BottomRight + my_BottomLeft);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 2:
+                    A = 0.5 * (my_TopRight + my_BottomRight);
+                    B = 0.5 * (my_BottomRight + my_BottomLeft);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 3:
+                    A = 0.5 * (my_TopLeft + my_BottomLeft);
+                    B = 0.5 * (my_TopRight + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 4:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_TopRight + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 5:
+                    A = 0.5 * (my_TopLeft + my_BottomLeft);
+                    B = 0.5 * (my_TopLeft + my_TopRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    A = 0.5 * (my_BottomLeft + my_BottomRight);
+                    B = 0.5 * (my_TopRight + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 6:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_BottomLeft + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 7:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_TopLeft + my_BottomLeft);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 8:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_TopLeft + my_BottomLeft);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 9:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_BottomLeft + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 10:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_TopRight + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    A = 0.5 * (my_TopLeft + my_BottomLeft);
+                    B = 0.5 * (my_BottomLeft + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 11:
+                    A = 0.5 * (my_TopLeft + my_TopRight);
+                    B = 0.5 * (my_TopRight + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 12:
+                    A = 0.5 * (my_TopLeft + my_BottomLeft);
+                    B = 0.5 * (my_TopRight + my_BottomRight);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 13:
+                    A = 0.5 * (my_TopRight + my_BottomRight);
+                    B = 0.5 * (my_BottomRight + my_BottomLeft);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 14:
+                    A = 0.5 * (my_TopLeft + my_BottomLeft);
+                    B = 0.5 * (my_BottomRight + my_BottomLeft);
+                    l = new Line()
+                    {
+                        X1 = A.X,
+                        Y1 = A.Y,
+                        X2 = B.X,
+                        Y2 = B.Y
+                    };
+                    result.Add(l);
+                    break;
+                case 15:
+                    //Do nothing
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
