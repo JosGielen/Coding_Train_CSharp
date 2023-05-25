4153999e43c1f698ece44ed132b9b9586979d445
JosGielen <joseph.gielen1@telenet.be>
2023-05-25 21:15:52 +0200

    Uploading Files

---
+++ CC001 Starfield/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Starfield
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Star> Stars;
+        private int StarCount = 800;
+        private double speed;
+        private Random rnd = new Random();
+        private bool Rendering = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Star s;
+            Stars = new List<Star>();
+            speed = SldSpeed.Value;
+            TxtSpeed.Text = speed.ToString();
+            for (int I = 0; I < StarCount; I++)
+            {
+                s = new Star(canvas1.ActualWidth * rnd.NextDouble(), canvas1.ActualHeight * rnd.NextDouble(), 3 * rnd.NextDouble());
+                s.Show(canvas1);
+                Stars.Add(s);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Rendering)
+            {
+                StartRender();
+            }
+            else
+            {
+                StopRender();
+            }
+        }
+
+        private void StartRender()
+        {
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+            BtnStart.Content = "FULL STOP";
+            Rendering = true;
+        }
+
+        private void StopRender()
+        {
+            CompositionTarget.Rendering -= CompositionTarget_Rendering;
+            BtnStart.Content = "ENGAGE";
+            Rendering = false;
+        }
+
+        private void CompositionTarget_Rendering(Object sender, EventArgs e)
+        {
+            for (int I = 0; I < Stars.Count; I++)
+            {
+                Stars[I].Update(speed);
+            }
+        }
+
+        private void SldSpeed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!IsLoaded) return;
+            speed = SldSpeed.Value;
+            TxtSpeed.Text = speed.ToString();
+        }
+    }
+}
---
+++ CC001 Starfield/Star.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Starfield
+{
+    internal class Star
+    {
+        private Point pos;
+        private double size;
+        private double maxWidth;
+        private double maxHeight;
+        private Ellipse dot;
+        private static Random rnd = new Random();
+
+        public Star(double x, double y, double starSize)
+        {
+            pos = new Point(x, y);
+            size = starSize;
+        }
+
+        public void Show(Canvas c)
+        {
+            dot = new Ellipse()
+            {
+                Width = size,
+                Height = size,
+                Stroke = Brushes.White,
+                StrokeThickness = 1.0,
+                Fill = Brushes.White
+            };
+            dot.SetValue(Canvas.LeftProperty, pos.X);
+            dot.SetValue(Canvas.TopProperty, pos.Y);
+            maxWidth = c.ActualWidth;
+            maxHeight = c.ActualHeight;
+            c.Children.Add(dot);
+        }
+
+        public void Update(double speed)
+        {
+            Vector v = pos - new Point(maxWidth / 2, maxHeight / 2); //From window center towards the current position
+            pos = pos + 0.01 * speed * v;
+            if (pos.X < 0 | pos.Y > maxWidth | pos.Y < 0 | pos.Y > maxHeight)
+            {
+                pos.X = 0.8 * maxWidth * rnd.NextDouble();
+                pos.Y = 0.8 * maxHeight * rnd.NextDouble();
+                dot.Width = 0;
+                dot.Height = 0;
+            }
+            dot.Width += 0.02 * speed;
+            dot.Height += 0.02 * speed;
+            dot.SetValue(Canvas.LeftProperty, pos.X);
+            dot.SetValue(Canvas.TopProperty, pos.Y);
+        }
+    }
+}
